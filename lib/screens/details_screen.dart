3d3c6ad1ce19852be15450377de954f3422bdec5
Fernando Salva <xxavierargentino@hotmail.com>
2021-11-15 20:20:37 -0300

    detalles terminado

--- lib/screens/details_screen.dart
+++ lib/screens/details_screen.dart
@@ -15,7 +15,8 @@ class DetailsScreen extends StatelessWidget {
           _CustomAppBar(
               '${personal.apellido1} ${personal.apellido2 ?? ''} ${personal.nombre1} ${personal.nombre2 ?? ''}'),
           SliverList(
-              delegate: SliverChildListDelegate([_PosterYData(personal)]))
+              delegate: SliverChildListDelegate(
+                  [_PosterYData(personal), _DetalleData(personal)]))
         ],
       ),
     );
@@ -65,18 +66,247 @@ class _PosterYData extends StatelessWidget {
           SizedBox(
             width: 10.0,
           ),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                'Grado: ${this._persona.grado.grado}',
-                style: Theme.of(context).textTheme.headline6,
-                overflow: TextOverflow.ellipsis,
-                maxLines: 2,
-              )
-            ],
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Grado: ${this._persona.grado.grado}',
+                  style: Theme.of(context).textTheme.headline6,
+                  overflow: TextOverflow.visible,
+                  maxLines: 2,
+                  textAlign: TextAlign.left,
+                ),
+                SizedBox(
+                  height: 10.0,
+                ),
+                Text(
+                  'Destino: ${this._persona.destino.destino}',
+                  style: Theme.of(context).textTheme.headline6,
+                  overflow: TextOverflow.visible,
+                  maxLines: 3,
+                  textAlign: TextAlign.left,
+                ),
+                SizedBox(
+                  height: 10.0,
+                ),
+                Text(
+                  'Departamento: ${this._persona.departamento.departamento}',
+                  style: Theme.of(context).textTheme.headline6,
+                  overflow: TextOverflow.visible,
+                  maxLines: 3,
+                  textAlign: TextAlign.left,
+                )
+              ],
+            ),
           )
         ],
+      ),
+    );
+  }
+}
+
+class _DetalleData extends StatelessWidget {
+  final PersonalElement _persona;
+
+  const _DetalleData(this._persona);
+
+  @override
+  Widget build(BuildContext context) {
+    String _situacion =
+        (this._persona.situacion.situacion.toString() == 'SitacionEnum.ACTIVO')
+            ? 'Activo'
+            : 'Pasivo';
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 20.0),
+      child: Expanded(
+        child: Column(
+          children: [
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Expanded(
+                    child: Text(
+                  "Situación: ${_situacion}",
+                  style: Theme.of(context).textTheme.headline6,
+                  overflow: TextOverflow.visible,
+                  maxLines: 3,
+                  textAlign: TextAlign.left,
+                )),
+                SizedBox(
+                  width: 10.0,
+                ),
+                Expanded(
+                    child: Text(
+                  "Escala Jerárquica: ${(this._persona.escalaJerarquica.escalaJerarquica.toString() == 'EscalaJerarquicaEnum.SUBOFICIAL') ? 'Suboficial' : 'Oficial'}",
+                  style: Theme.of(context).textTheme.headline6,
+                  overflow: TextOverflow.visible,
+                  maxLines: 3,
+                  textAlign: TextAlign.left,
+                ))
+              ],
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Expanded(
+                    child: Text(
+                  "Escalafón: ${(this._persona.escalafon.escalafon.toString() == 'EscalafonEnum.PENITENCIARIO') ? 'Penitenciario' : 'Profesional y Técnico'}",
+                  style: Theme.of(context).textTheme.headline6,
+                  overflow: TextOverflow.visible,
+                  maxLines: 3,
+                  textAlign: TextAlign.left,
+                )),
+                SizedBox(
+                  width: 10.0,
+                ),
+                Expanded(
+                    child: Text(
+                  "Domicilio: ${(this._persona.domicilio)}",
+                  style: Theme.of(context).textTheme.headline6,
+                  overflow: TextOverflow.visible,
+                  maxLines: 5,
+                  textAlign: TextAlign.left,
+                ))
+              ],
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Expanded(
+                    child: Text(
+                  //"Estado Civil: ${this._persona.estadoCivil.estadoCivil}",
+                  "Estado Civil: ${(this._persona.estadoCivil.estadoCivil.toString() == 'EstadoCivilEnum.CASADO') ? 'Casado' : 'Soltero'}",
+                  style: Theme.of(context).textTheme.headline6,
+                  overflow: TextOverflow.visible,
+                  maxLines: 3,
+                  textAlign: TextAlign.left,
+                )),
+                SizedBox(
+                  width: 10.0,
+                ),
+                Expanded(
+                    child: Text(
+                  "Correo: ${(this._persona.email)}",
+                  style: Theme.of(context).textTheme.headline6,
+                  overflow: TextOverflow.visible,
+                  maxLines: 5,
+                  textAlign: TextAlign.left,
+                ))
+              ],
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Expanded(
+                    child: Text(
+                  //"Estado Civil: ${this._persona.estadoCivil.estadoCivil}",
+                  "Fecha de Nacimiento: ${this._persona.fechaNacimiento}",
+                  style: Theme.of(context).textTheme.headline6,
+                  overflow: TextOverflow.visible,
+                  maxLines: 3,
+                  textAlign: TextAlign.left,
+                )),
+                SizedBox(
+                  width: 10.0,
+                ),
+                Expanded(
+                    child: Text(
+                  "Legajo: ${(this._persona.legajo)}",
+                  style: Theme.of(context).textTheme.headline6,
+                  overflow: TextOverflow.visible,
+                  maxLines: 5,
+                  textAlign: TextAlign.left,
+                ))
+              ],
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Expanded(
+                    child: Text(
+                  //"Estado Civil: ${this._persona.estadoCivil.estadoCivil}",
+                  "Peso: ${this._persona.peso}",
+                  style: Theme.of(context).textTheme.headline6,
+                  overflow: TextOverflow.visible,
+                  maxLines: 3,
+                  textAlign: TextAlign.left,
+                )),
+                SizedBox(
+                  width: 10.0,
+                ),
+                Expanded(
+                    child: Text(
+                  "Altura: ${(this._persona.altura)}",
+                  style: Theme.of(context).textTheme.headline6,
+                  overflow: TextOverflow.visible,
+                  maxLines: 5,
+                  textAlign: TextAlign.left,
+                ))
+              ],
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Expanded(
+                    child: Text(
+                  //"Estado Civil: ${this._persona.estadoCivil.estadoCivil}",
+                  "Nivel Educativo: ${(this._persona.nivelEducativo.nivelEducativo.toString() == 'NivelEducativoEnum.SECUNDARIA_COMPLETA') ? 'Secundario Completo' : 'Terciario Incompleto'}",
+                  style: Theme.of(context).textTheme.headline6,
+                  overflow: TextOverflow.visible,
+                  maxLines: 3,
+                  textAlign: TextAlign.left,
+                )),
+                SizedBox(
+                  width: 10.0,
+                ),
+                Expanded(
+                    child: Text(
+                  "Funciones: ${(this._persona.funcion)}",
+                  style: Theme.of(context).textTheme.headline6,
+                  overflow: TextOverflow.visible,
+                  maxLines: 5,
+                  textAlign: TextAlign.left,
+                ))
+              ],
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            Text(
+              'Ut eiusmod sunt irure dolor ullamco proident deserunt nisi irure. Nisi sint exercitation ipsum sit ex occaecat ex dolor nulla est labore nostrud elit. Non quis officia amet nostrud magna excepteur ea nulla duis.',
+              style: Theme.of(context).textTheme.headline6,
+              // overflow: TextOverflow.visible,
+              // maxLines: 3,
+              textAlign: TextAlign.justify,
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            Text(
+              'Ut eiusmod sunt irure dolor ullamco proident deserunt nisi irure. Nisi sint exercitation ipsum sit ex occaecat ex dolor nulla est labore nostrud elit. Non quis officia amet nostrud magna excepteur ea nulla duis.',
+              style: Theme.of(context).textTheme.headline6,
+              // overflow: TextOverflow.visible,
+              // maxLines: 3,
+              textAlign: TextAlign.justify,
+            ),
+          ],
+        ),
       ),
     );
   }
